4a5307101e6c892f0ee7f26e41a63bcea65953a2
Tien Huynh <mineshark15@gmail.com>
2024-08-02 00:15:29 +0700

    update od, place code

--- utils/search_by_od.py
+++ utils/search_by_od.py
@@ -1,12 +1,15 @@
-def search_frames_with_all_objects(query_objects, data, frame_ids):
+def search_frames_with_all_objects(query_objects, data, frame_ids, json_dict):
+    print(json_dict)
     matching_frame_indexes = []
-    
+    not_matching_frame_indexes = []
     for frame_id in frame_ids:
-        frame_data = data.get(frame_id, {})
-        frame_objects = set(frame_data.get('objects', []))
+        frame_objects = [frame_object.lower() for frame_object in data[json_dict[str(frame_id)].split("/")[-1][:-4]]['objects']]
         
         # Kiểm tra nếu tất cả các đối tượng trong query_objects có trong frame_objects
         if all(obj.lower() in frame_objects for obj in query_objects):
             matching_frame_indexes.append(frame_id)
-    
-    return matching_frame_indexes
+        else:
+            not_matching_frame_indexes.append(frame_id)
+    print(matching_frame_indexes)
+    id_list = matching_frame_indexes + not_matching_frame_indexes
+    return id_list
