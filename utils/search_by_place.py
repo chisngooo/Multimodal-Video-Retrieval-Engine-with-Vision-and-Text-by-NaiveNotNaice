aba79f8773fdb07538fcce64da5fa4af1983a1ed
Tung Le Thanh <139512238+Zhennor@users.noreply.github.com>
2024-08-01 14:27:45 +0700

    Add files via upload

--- utils/search_by_place.py
+++ utils/search_by_place.py
@@ -1,9 +1,13 @@
 def search_frames_with_any_place(query_place, data, frame_ids, json_dict):
     matching_frame_indexes = []
+    not_matching_frame_indexes = []
     for frame_id in frame_ids:
         frame_place = data[json_dict[str(frame_id)].split("/")[-1][:-4]]['place'].lower()
         # Kiểm tra nếu query_place có xuất hiện trong frame_place
         print(query_place, frame_place)
         if query_place.lower() in frame_place:
             matching_frame_indexes.append(frame_id)
-    return matching_frame_indexes
+        else :
+            not_matching_frame_indexes.append(frame_id)
+    id_list = matching_frame_indexes + not_matching_frame_indexes
+    return id_list
