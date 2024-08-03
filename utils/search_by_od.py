4e21910a56880d56738a65e98ef313acf038b5c9
Tung Le Thanh <139512238+Zhennor@users.noreply.github.com>
2024-08-04 00:23:55 +0700

    Add files via upload

--- utils/search_by_od.py
+++ utils/search_by_od.py
@@ -1,15 +1,56 @@
-def search_frames_with_all_objects(query_objects, data, frame_ids, json_dict):
-    print(json_dict)
-    matching_frame_indexes = []
-    not_matching_frame_indexes = []
-    for frame_id in frame_ids:
-        frame_objects = [frame_object.lower() for frame_object in data[json_dict[str(frame_id)].split("/")[-1][:-4]]['objects']]
+from elasticsearch import Elasticsearch
+
+
+def search_od(query, index_name, size):
+    # Tạo điều kiện truy vấn cho từng từ khóa trong mảng
+    es = Elasticsearch(["http://localhost:9200"])
+    must_conditions = [{"match": {"objects": keyword}} for keyword in query]
+
+    # Tìm kiếm dữ liệu với nhiều điều kiện match
+    search_query = {
+        "query": {
+            "bool": {
+                "must": must_conditions
+            }
+        },
+        "size": size  # Trả về số lượng kết quả tùy ý
+    }
+
+    response = es.search(index=index_name, body=search_query)
+    hits = response['hits']['hits']
+
+    # Lấy các ID khớp
+    matching_ids = [hit['_id'] for hit in hits]
+    num_matching = len(matching_ids)
+
+    if num_matching < size:
+        # Truy vấn để lấy các tài liệu không khớp hoặc có điểm số thấp
+        additional_query = {
+            "query": {
+                "bool": {
+                    "must_not": [
+                        {
+                            "ids": {
+                                "values": matching_ids
+                            }
+                        }
+                    ]
+                }
+            },
+            "size": size - num_matching
+        }
         
-        # Kiểm tra nếu tất cả các đối tượng trong query_objects có trong frame_objects
-        if all(obj.lower() in frame_objects for obj in query_objects):
-            matching_frame_indexes.append(frame_id)
-        else:
-            not_matching_frame_indexes.append(frame_id)
-    print(matching_frame_indexes)
-    id_list = matching_frame_indexes + not_matching_frame_indexes
-    return id_list
+        additional_response = es.search(index=index_name, body=additional_query)
+        additional_hits = additional_response['hits']['hits']
+        
+        # Lấy các ID của tài liệu bổ sung
+        additional_ids = [hit['_id'] for hit in additional_hits]
+        matching_ids.extend(additional_ids)  # Thêm các ID bổ sung vào danh sách kết quả
+    else:
+        # Nếu số lượng tài liệu khớp đủ
+        matching_ids = matching_ids
+    
+    return matching_ids
+
+
+
--- utils/search_by_place.py
+++ utils/search_by_place.py
@@ -1,13 +1,55 @@
-def search_frames_with_any_place(query_place, data, frame_ids, json_dict):
-    matching_frame_indexes = []
-    not_matching_frame_indexes = []
-    for frame_id in frame_ids:
-        frame_place = data[json_dict[str(frame_id)].split("/")[-1][:-4]]['place'].lower()
-        # Kiểm tra nếu query_place có xuất hiện trong frame_place
-        print(query_place, frame_place)
-        if query_place.lower() in frame_place:
-            matching_frame_indexes.append(frame_id)
-        else :
-            not_matching_frame_indexes.append(frame_id)
-    id_list = matching_frame_indexes + not_matching_frame_indexes
-    return id_list
+from elasticsearch import Elasticsearch
+
+def search_place(query, index_name, size):
+    # Kết nối đến Elasticsearch
+    es = Elasticsearch(["http://localhost:9200"])
+
+    # Tạo truy vấn tìm kiếm với một từ khóa đơn lẻ
+    search_query = {
+        "query": {
+            "match": {
+                "place": {
+                    "query": query,
+                    "fuzziness": "AUTO"  # Tùy chọn để cho phép khớp không chính xác
+                }
+            }
+        },
+        "size": size  # Trả về số lượng kết quả tùy ý
+    }
+
+    response = es.search(index=index_name, body=search_query)
+    hits = response['hits']['hits']
+
+    # Lấy các ID khớp
+    matching_ids = [hit['_id'] for hit in hits]
+    num_matching = len(matching_ids)
+
+    if num_matching < size:
+        # Truy vấn để lấy các tài liệu không khớp hoặc có điểm số thấp
+        additional_query = {
+            "query": {
+                "bool": {
+                    "must_not": [
+                        {
+                            "ids": {
+                                "values": matching_ids
+                            }
+                        }
+                    ]
+                }
+            },
+            "size": size - num_matching
+        }
+        
+        additional_response = es.search(index=index_name, body=additional_query)
+        additional_hits = additional_response['hits']['hits']
+        
+        # Lấy các ID của tài liệu bổ sung
+        additional_ids = [hit['_id'] for hit in additional_hits]
+        matching_ids.extend(additional_ids)  # Thêm các ID bổ sung vào danh sách kết quả
+    else:
+        # Nếu số lượng tài liệu khớp đủ
+        matching_ids = matching_ids
+    
+    return matching_ids
+
