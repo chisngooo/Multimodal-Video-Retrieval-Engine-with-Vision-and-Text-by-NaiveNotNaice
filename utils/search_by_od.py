1f3c1be75481230f752111571068e4d538451bce
Tung Le Thanh <139512238+Zhennor@users.noreply.github.com>
2024-08-06 00:28:57 +0700

    Add files via upload

--- utils/search_by_od.py
+++ utils/search_by_od.py
@@ -1,6 +1,5 @@
 from elasticsearch import Elasticsearch
 
-
 def search_od(query, index_name, size):
     # Tạo điều kiện truy vấn cho từng từ khóa trong mảng
     es = Elasticsearch(["http://localhost:9200"])
@@ -39,18 +38,13 @@ def search_od(query, index_name, size):
             },
             "size": size - num_matching
         }
-        
+
         additional_response = es.search(index=index_name, body=additional_query)
         additional_hits = additional_response['hits']['hits']
-        
-        # Lấy các ID của tài liệu bổ sung
         additional_ids = [hit['_id'] for hit in additional_hits]
         matching_ids.extend(additional_ids)  # Thêm các ID bổ sung vào danh sách kết quả
     else:
         # Nếu số lượng tài liệu khớp đủ
         matching_ids = matching_ids
-    
+
     return matching_ids
-
-
-
